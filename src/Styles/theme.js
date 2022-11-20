e3bf96865e945ab46e6294f61de09768d3970a80
danilosb91 <danilosilva.barao@gmail.com>
2022-11-20 18:08:25 -0300

    Project made with ReactJS

---
+++ src/Styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_100: "#948F99",
+    BACKGROUND_200: "#312E38",
+    BACKGROUND_300: "#1C1B1E",
+    BACKGROUND_400: "#0D0C0F",
+
+    ROSE: "#FF859B",
+    WHITE: "#F4EDE8",
+
+    BORDER: "#948F99",
+
+    INPUT: "#262529",
+  },
+};
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./Styles/global";
+import theme from "./Styles/theme";
+import { Home } from "./Pages/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
+  </React.StrictMode>
+);
